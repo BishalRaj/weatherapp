4dc99c95797c3439ca52d1fac20a634dee253a5b
BishalRaj <bisal1shakya@gmail.com>
2023-02-17 19:33:12 +0000

    added cloud mesh

--- src/components/earth/index.tsx
+++ src/components/earth/index.tsx
@@ -7,6 +7,10 @@ import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import { TextureLoader } from "three";
 import { OrbitControls } from "@react-three/drei";
+
+import * as THREE from 'three'
+
+
 export function Earth() {
   const [colorMap, normalMap, specularMap, cloudMap] = useLoader(
     TextureLoader,
@@ -15,12 +19,16 @@ export function Earth() {
 
   return (
     <>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.7} />
+      <mesh>
+        <sphereGeometry args={[1.005,32,32]} />
+        <meshPhongMaterial map={cloudMap} opacity={0.4} depthWrite={true} transparent={true} side={THREE.DoubleSide} />
+      </mesh>
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         {/* <meshPhongMaterial color="blue" /> */}
         <meshStandardMaterial map={colorMap} normalMap={normalMap} />
-         <OrbitControls enableZoom={true} />
+         <OrbitControls enableZoom={true} enableRotate={true} zoomSpeed={0.6} panSpeed={0.5} rotateSpeed={0.4} />
       </mesh>
     </>
   );
